fe7a6a6beafd172c1ae0abb57a9b16c1d0ba36f8
pragya.srivastava3888@gmail.com <49591103+pragya2404@users.noreply.github.com>
2020-12-28 21:18:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express=require('express')
+const mongoose=require('mongoose')
+// const connectDB=require('./config/db')
+const ShortUrl=require('./models/shortUrl')
+const app=express()
+
+//connect to database
+//   connectDB();
+
+const PORT=5500
+app.listen(PORT,()=>console.log(`server is started at server ${PORT}`))
+
+mongoose.connect('mongodb://localhost/urlShortener',{
+    useNewUrlParser:true,useUnifiedTopology:true
+})
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:false}))
+app.get('/',async (req,res)=>{
+    const shortUrls= await ShortUrl.find()
+      res.render('index',{shortUrls:shortUrls})
+})
+
+app.post('/shortUrls',async (req,res)=>{
+    await ShortUrl.create({full:req.body.fullUrl})
+
+    res.redirect('/')
+})
+
+app.get('/:shortUrl',async (req,res)=>{
+  const shortUrl=await ShortUrl.findOne({short:req.params.shortUrl})
+  if(shortUrl==null)return res.sendStatus(404)
+  
+  shortUrl.clicks++;
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
