eea550b7613eaad0cc51b4d7a3f7f8218f444ebd
pragya2404 <49591103+pragya2404@users.noreply.github.com>
2021-01-28 21:51:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express=require('express')
-const mongoose=require('mongoose')
 const connectDB=require('./config/db')
 const ShortUrl=require('./models/shortUrl')
 const app=express()
@@ -11,7 +10,9 @@ const PORT=5500
 app.listen(PORT,()=>console.log(`server is started at server ${PORT}`))
 
 app.set('view engine','ejs')
+
 app.use(express.urlencoded({extended:false}))
+
 app.get('/',async (req,res)=>{
     const shortUrls= await ShortUrl.find()
       res.render('index',{shortUrls:shortUrls})
@@ -28,7 +29,7 @@ app.get('/:shortUrl',async (req,res)=>{
   if(shortUrl==null)return res.sendStatus(404)
   
   shortUrl.clicks++;
-  shortUrl.save()
+  shortUrl.save();
 
   res.redirect(shortUrl.full)
 })
